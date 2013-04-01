3ef17692ee7829352c8e0f8203339de1852d3902
Karthik Ram <karthik.ram@gmail.com>
2013-04-01 12:28:03 -0700

    removing simple_cap function

--- impact_report.R
+++ impact_report.R
@@ -9,36 +9,56 @@ suppressPackageStartupMessages(library(plyr))
 # If your impactstory key is not stored in your .rprofile, add it below with key =
 # metrics <- collection_metrics(collection_id = impact_report_id, key = 'YOUR_KEY')
 metrics <- collection_metrics(collection_id = impact_report_id)
-title <- metrics[[9]]
+title <- metrics[[which(names(metrics)=="title")]]
 # From all the fields returned, I just need the metrics
-data <- metrics[[5]]
+data <- metrics[[which(names(metrics)=="items")]]
+
 tabular_data <- llply(data, function(x) {
+    bookmarks_data <- data.frame()
+    stars_data <- data.frame()
+    fork_data <- data.frame()
+    tweet_data <- data.frame()
     # stars
-    stars_metrics <- data.frame(x$metrics$`github:stars`$values$github)
+    stars_pos <- which(names(x$metrics$`github:stars`$values)=="github")
+    if(length(stars_pos) > 0) {
+        stars_metrics <- data.frame(x$metrics$`github:stars`$values$github)
     stars_data <- data.frame(count = x$metrics$`github:stars`$values$raw,
         t(data.frame(stars_metrics)))
     rownames(stars_data) <- NULL
+    }
     # tweets
+    tweets_pos <- which(names(x$metrics$`topsy:tweets`$values)=="github")
+    if(length(tweets_pos) > 0) {
     tweet_metrics <- t(data.frame(x$metrics$`topsy:tweets`$values$github))
     tweet_data <- data.frame(count = x$metrics$`topsy:tweets`$values$raw,
         tweet_metrics)
     rownames(tweet_data) <- NULL
+    }
+
     # bookmarks
+     bookmarks_pos <- which(names(x$metrics$`delicious:bookmarks`$values)=="github")
+    if(length(bookmarks_pos) > 0) {
     bookmarks_metrics <- t(data.frame(x$metrics$`delicious:bookmarks`$values$github))
     bookmarks_data <- data.frame(count = x$metrics$`delicious:bookmarks`$values$raw,
         bookmarks_metrics)
     rownames(bookmarks_data) <- NULL
+}
     # forks
+    forks_pos <- which(names(x$metrics$`github:forks`$values)=="github")
+    if(length(forks_pos) > 0) {
     forks_metrics <- t(data.frame(x$metrics$`github:forks`$values$github))
     fork_data <- data.frame(count = x$metrics$`github:forks`$values$raw,
         forks_metrics)
     rownames(fork_data) <- NULL
+    }
     # and finally the metadata
     results <- data.frame(title = x$biblio[[6]], URL = x$aliases$url,
         description = x$biblio[[2]], year = x$biblio[[8]])
-    return(list(results, stars = stars_data, tweets = tweet_data,
-        bookmarks = bookmarks_data, forks = fork_data))
+    return(list(results, Stars = stars_data, Tweets = tweet_data,
+        Bookmarks = bookmarks_data, Forks = fork_data))
 })
+
+
 github_data <- function(foo) {
     data <- ldply(foo[2:length(foo)])
     names(data)[1] <- "metric"
@@ -49,8 +69,4 @@ github_data <- function(foo) {
     data$year <- info[1, ]$year
     return(data)
 }
-simpleCap <- function(x) {
-    foo <- paste(toupper(substring(x, 1, 1)), substring(x, 2, nchar(x)),
-        sep = "", collapse = " ")
-    return(foo)
-}
+
